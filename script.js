9c1ba08c968fcde315bc09eab7894071e1f5d99b
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2020-10-09 00:39:18 -0700

    added prompts

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -11,6 +8,39 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+
+function generatePassword() {
+  // prompt: what is the length?
+  var size = window.prompt("How many characters would you like your password to be? Length must be between 8-128 characters.", "8");
+  console.log("size", size, typeof size);
+  //    valid values are >=8 && <=128
+
+  // prompt: what character types to be included in password?
+  //    lowercase
+  var lowercase = confirm("Click OK to include a lowercase character in your password.");
+  console.log("lowercase", lowercase, typeof lowercase)
+
+  //    uppercase
+  var uppercase = confirm("Click OK to include an uppercase character in your password.");
+  console.log("uppercase", uppercase, typeof uppercase)
+
+  //    numeric
+  var numeric = confirm("Click OK to include a numeric character in your password.");
+  console.log("numeric", numeric, typeof numeric)
+
+  //    special characters
+  var special = confirm("Click OK to include a special character in your password.");
+  console.log("special", special, typeof special)
+
+  // when each prompt is answered, input should be validated and at least one character must be selected
+
+  // generate a password
+
+
+
+  return 0;
 }
 
 // Add event listener to generate button
